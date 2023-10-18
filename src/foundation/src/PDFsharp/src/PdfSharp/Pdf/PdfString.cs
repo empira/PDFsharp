adaf57e3d040b19569588000dbd4338d426e6def
Julien R <j.raffray@gmail.com>
2023-10-18 15:19:02 +0200

    expose PdfString's start and end bytes positions in Stream
    This will make possible the implementation of document signature, because we need to exclude this specific byte range from the hash digest.

--- src/foundation/src/PDFsharp/src/PdfSharp/Pdf/PdfString.cs
+++ src/foundation/src/PDFsharp/src/PdfSharp/Pdf/PdfString.cs
@@ -284,7 +284,21 @@ namespace PdfSharp.Pdf
         /// </summary>
         internal override void WriteObject(PdfWriter writer)
         {
+            PositionStart = writer.Position;
+
             writer.Write(this);
+
+            PositionEnd = writer.Position;
         }
+
+        /// <summary>
+        /// Position of the first byte of this string in PdfWriter's Stream
+        /// </summary>
+        public int PositionStart { get; internal set; }
+
+        /// <summary>
+        /// Position of the last byte of this string in PdfWriter's Stream
+        /// </summary>
+        public int PositionEnd { get; internal set; }
     }
 }
