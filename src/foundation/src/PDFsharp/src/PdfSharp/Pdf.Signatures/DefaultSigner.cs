55ddebb7e38d284d788e4062804e3bdcd95fc94c
Julien R <j.raffray@gmail.com>
2024-02-23 17:05:36 +0100

    DefaultSigner: add the ability to attach a signature timestamp from TSA

--- src/foundation/src/PDFsharp/src/PdfSharp/Pdf.Signatures/DefaultSigner.cs
+++ src/foundation/src/PDFsharp/src/PdfSharp/Pdf.Signatures/DefaultSigner.cs
@@ -4,6 +4,8 @@
 #if WPF
 using System.IO;
 #endif
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
 using System.Security.Cryptography.Pkcs;
 using System.Security.Cryptography.X509Certificates;
 
@@ -11,27 +13,36 @@ namespace PdfSharp.Pdf.Signatures
 {
     public class DefaultSigner : ISigner
     {
-        public X509Certificate2 Certificate { get; private set; }
+        private static readonly Oid SignatureTimeStampOin = new Oid("1.2.840.113549.1.9.16.2.14");
+        private static readonly string TimestampQueryContentType = "application/timestamp-query";
+        private static readonly string TimestampReplyContentType = "application/timestamp-reply";
 
-        public DefaultSigner(X509Certificate2 Certificate)
+        private X509Certificate2 _certificate { get; init; }
+        private Uri? _timeStampAuthorityUri { get; init; }
+
+        public DefaultSigner(X509Certificate2 Certificate, Uri? timeStampAuthorityUri = null)
         {
-            this.Certificate = Certificate;
+            _certificate = Certificate;
+            _timeStampAuthorityUri = timeStampAuthorityUri;
         }
 
         public byte[] GetSignedCms(Stream stream, int pdfVersion)
         {
             var range = new byte[stream.Length];
-
             stream.Position = 0;
             stream.Read(range, 0, range.Length);
 
+            // Sign the byte range
             var contentInfo = new ContentInfo(range);
-
             SignedCms signedCms = new SignedCms(contentInfo, true);
-            CmsSigner signer = new CmsSigner(Certificate);
+            CmsSigner signer = new CmsSigner(_certificate)/* { IncludeOption = X509IncludeOption.WholeChain }*/;
             signer.UnsignedAttributes.Add(new Pkcs9SigningTime());
 
             signedCms.ComputeSignature(signer, true);
+
+            if (_timeStampAuthorityUri is not null)
+                Task.Run(() => AddTimestampFromTSAAsync(signedCms)).Wait();
+
             var bytes = signedCms.Encode();
 
             return bytes;
@@ -39,7 +50,48 @@ namespace PdfSharp.Pdf.Signatures
 
         public string GetName()
         {
-            return Certificate.GetNameInfo(X509NameType.SimpleName, false);
+            return _certificate.GetNameInfo(X509NameType.SimpleName, false);
+        }
+
+        private async Task AddTimestampFromTSAAsync(SignedCms signedCms)
+        {
+            // Generate our nonce to identify the pair request-response
+            byte[] nonce = new byte[8];
+#if NET6_0_OR_GREATER
+            nonce = RandomNumberGenerator.GetBytes(8);
+#else
+            using var cryptoProvider = new RNGCryptoServiceProvider();
+            cryptoProvider.GetBytes(nonce = new Byte[8]);
+#endif
+            // Get our signing information and create the RFC3161 request
+            SignerInfo newSignerInfo = signedCms.SignerInfos[0];
+            // Now we generate our request for us to send to our RFC3161 signing authority.
+            var request = Rfc3161TimestampRequest.CreateFromSignerInfo(
+                newSignerInfo,
+                HashAlgorithmName.SHA256,
+                requestSignerCertificates: true, // ask TSA to embed its signing certificate in the timestamp token
+                nonce: nonce);
+
+            var client = new HttpClient();
+            var content = new ReadOnlyMemoryContent(request.Encode());
+            content.Headers.ContentType = new MediaTypeHeaderValue(TimestampQueryContentType);
+            var httpResponse = await client.PostAsync(_timeStampAuthorityUri, content).ConfigureAwait(false);
+
+            // Process our response
+            if (!httpResponse.IsSuccessStatusCode)
+            {
+                throw new CryptographicException(
+                    $"There was a error from the timestamp authority. It responded with {httpResponse.StatusCode} {(int)httpResponse.StatusCode}: {httpResponse.Content}");
+            }
+            if (httpResponse.Content.Headers.ContentType.MediaType != TimestampReplyContentType)
+            {
+                throw new CryptographicException("The reply from the time stamp server was in a invalid format.");
+            }
+            var data = await httpResponse.Content.ReadAsByteArrayAsync().ConfigureAwait(false);
+            var timestampToken = request.ProcessResponse(data, out _);
+
+            // The RFC3161 sign certificate is separate to the contents that was signed, we need to add it to the unsigned attributes.
+            newSignerInfo.AddUnsignedAttribute(new AsnEncodedData(SignatureTimeStampOin, timestampToken.AsSignedCms().Encode()));
         }
     }
 }
