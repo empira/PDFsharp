d139393da4e49ce980917307bbe4c5f3c4e83f03
Audionysos <audionysoss@gmail.com>
2024-03-02 01:29:58 +0100

    Add Value and this[string] to the PdfItem. Implemented methods used for this[string] in PdfDictionary.

--- src/foundation/src/PDFsharp/src/PdfSharp/Pdf/PdfItem.cs
+++ src/foundation/src/PDFsharp/src/PdfSharp/Pdf/PdfItem.cs
@@ -2,6 +2,7 @@
 // See the LICENSE file in the solution root for more information.
 
 using PdfSharp.Pdf.IO;
+using PdfSharp.Pdf.Advanced;
 
 namespace PdfSharp.Pdf
 {
@@ -10,9 +11,25 @@ namespace PdfSharp.Pdf
     /// </summary>
     public abstract class PdfItem : ICloneable
     {
-        // All simple types (i.e. derived from PdfItem but not from PdfObject) must be immutable.
+        /// <summary>Returns this object or if this object is a reference, returns <see cref="PdfReference.Value"/> of the reference.</summary>
+        public PdfItem Value => this is PdfReference r ? r.Value : this;
+        /// <summary>Returns or creates child with given name.
+        /// Note that implementation for this may be provided by specific descendants of the <see cref="PdfItem"/> class but it's not mandatory.
+        /// If implementation is not provided by specific type a <see cref="NotSupportedException"/> will be thrown when attempting to use this accessors.</summary>
+        public PdfItem this[string name] {
+            get => Value.getChildByName(name);
+            set => Value.addChildWithName(name, value);
+        }
 
-        object ICloneable.Clone()
+		protected virtual PdfItem getChildByName(String name)
+            => throw new NotSupportedException($@"{this.GetType()} does not allow getting children by name.");
+		protected virtual void addChildWithName(String name, PdfItem value)
+            => throw new NotSupportedException($@"{this.GetType()} does not allow adding children.");
+
+
+		// All simple types (i.e. derived from PdfItem but not from PdfObject) must be immutable.
+
+		object ICloneable.Clone()
         {
             return Copy();
         }
